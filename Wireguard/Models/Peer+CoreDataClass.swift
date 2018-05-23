c5237d534270821cbe426693497cf19e99e16022
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2018-05-23 22:13:09 +0200

    Add data model.
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

---
+++ Wireguard/Models/Peer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Peer+CoreDataClass.swift
+//  
+//
+//  Created by Jeroen Leenarts on 23-05-18.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Peer)
+public class Peer: NSManagedObject {
+
+}
--- Wireguard/Models/Profile+CoreDataProperties.swift
+++ Wireguard/Models/Profile+CoreDataProperties.swift
@@ -10,10 +10,32 @@
 import Foundation
 import CoreData
 
+
 extension Profile {
 
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Profile> {
         return NSFetchRequest<Profile>(entityName: "Profile")
     }
+
+    @NSManaged public var title: String?
+    @NSManaged public var peers: NSSet?
+    @NSManaged public var interface: Interface?
+
+}
+
+// MARK: Generated accessors for peers
+extension Profile {
+
+    @objc(addPeersObject:)
+    @NSManaged public func addToPeers(_ value: Peer)
+
+    @objc(removePeersObject:)
+    @NSManaged public func removeFromPeers(_ value: Peer)
+
+    @objc(addPeers:)
+    @NSManaged public func addToPeers(_ values: NSSet)
+
+    @objc(removePeers:)
+    @NSManaged public func removeFromPeers(_ values: NSSet)
 
 }
