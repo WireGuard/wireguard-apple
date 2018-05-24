d2dd61cf0c18d2b55eb06367dbce566863fd1325
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2018-05-24 06:21:46 +0200

    Add wg-quick related model properties.
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

---
+++ Wireguard/Models/Address+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Address+CoreDataClass.swift
+//  Wireguard
+//
+//  Created by Jeroen Leenarts on 24-05-18.
+//  Copyright © 2018 Wireguard. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Address)
+public class Address: NSManagedObject {
+
+}
--- Wireguard/Models/Profile+CoreDataProperties.swift
+++ Wireguard/Models/Profile+CoreDataProperties.swift
@@ -10,7 +10,6 @@
 import Foundation
 import CoreData
 
-
 extension Profile {
 
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Profile> {
