bb1e05f9db7e0433cfcb7a8678fe9b034c3a0abe
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2018-06-22 08:23:39 +0200

    Objective-C wrapper around WireguardGo.

---
+++ WireGuardNetworkExtension/WireGuardGoWrapper.h
@@ -0,0 +1,16 @@
+//
+//  WireGuardGoWrapper.h
+//  WireGuardNetworkExtension
+//
+//  Created by Jeroen Leenarts on 21-06-18.
+//  Copyright © 2018 Wireguard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WireGuardGoWrapper : NSObject
+
+- (void) turnOnWithInterfaceName: (NSString *)interfaceName settingsString: (NSString *)settingsString;
+- (void) turnOff;
+
+@end
