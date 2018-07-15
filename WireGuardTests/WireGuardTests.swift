0f1aa701dff6bb955023375a5592b21f459769ac
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2018-07-15 11:55:41 +0200

    Change copyright notice.

--- WireGuardTests/WireGuardTests.swift
+++ WireGuardTests/WireGuardTests.swift
@@ -3,7 +3,7 @@
 //  WireGuardTests
 //
 //  Created by Jeroen Leenarts on 23-05-18.
-//  Copyright © 2018 WireGuard. All rights reserved.
+//  Copyright © 2018 Jason A. Donenfeld <Jason@zx2c4.com>. All rights reserved.
 //
 
 import XCTest
