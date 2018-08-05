2307bf25b5539285add4eb1f9a68dceb58fb6da0
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2018-08-05 07:48:36 +0200

    Assign correct copyright.
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- WireGuard/Models/Tunnel+Extension.swift
+++ WireGuard/Models/Tunnel+Extension.swift
@@ -3,7 +3,7 @@
 //  WireGuard
 //
 //  Created by Jeroen Leenarts on 04-08-18.
-//  Copyright © 2018 WireGuard. All rights reserved.
+//  Copyright © 2018 Jason A. Donenfeld <Jason@zx2c4.com>. All rights reserved.
 //
 
 import Foundation
