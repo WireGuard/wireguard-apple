d15a2ee88ebe2e0db0b37a7579e4c9184edf1eb2
Roopesh Chander <roop@roopc.net>
2018-08-11 13:19:21 +0530

    Remove storyboard and create UI from code

---
+++ WireGuard/WireGuard/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  WireGuard
+//
+//  Created by Roopesh Chander on 11/08/18.
+//  Copyright © 2018 Roopesh Chander. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func loadView() {
+        self.view = UIView()
+    }
+}
