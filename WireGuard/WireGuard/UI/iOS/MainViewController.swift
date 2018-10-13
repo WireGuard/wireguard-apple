9443ee7e30b014aedb80c6235b95b10397f4f782
Roopesh Chander <roop@roopc.net>
2018-10-13 16:02:42 +0530

    Configure the split-view controller to work in both iPhone and iPad

--- WireGuard/WireGuard/UI/iOS/MainViewController.swift
+++ WireGuard/WireGuard/UI/iOS/MainViewController.swift
@@ -20,4 +20,28 @@ class MainViewController: UISplitViewController {
 
         super.loadView()
     }
+
+    override func viewDidLoad() {
+        self.delegate = self
+
+        // On iPad, always show both masterVC and detailVC, even in portrait mode, like the Settings app
+        self.preferredDisplayMode = .allVisible
+    }
+}
+
+extension MainViewController: UISplitViewControllerDelegate {
+    func splitViewController(_ splitViewController: UISplitViewController,
+                             collapseSecondary secondaryViewController: UIViewController,
+                             onto primaryViewController: UIViewController) -> Bool {
+        // On iPhone, if the secondaryVC (detailVC) is just a UIViewController, it indicates that it's empty,
+        // so just show the primaryVC (masterVC).
+        let detailVC = (secondaryViewController as? UINavigationController)?.viewControllers.first
+        let isDetailVCEmpty: Bool
+        if let detailVC = detailVC {
+            isDetailVCEmpty = (type(of: detailVC) == UIViewController.self)
+        } else {
+            isDetailVCEmpty = true
+        }
+        return isDetailVCEmpty
+    }
 }
