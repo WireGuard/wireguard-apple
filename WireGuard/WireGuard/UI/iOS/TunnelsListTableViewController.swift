0c7321e92313b594ab6ee600a214fa59fa2b56a2
Roopesh Chander <roop@roopc.net>
2018-10-13 07:08:30 +0530

    Use a split-view controller as the main (root) view controller

---
+++ WireGuard/WireGuard/UI/iOS/TunnelsListTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TunnelsListTableViewController.swift
+//  WireGuard
+//
+//  Created by Roopesh Chander on 12/10/18.
+//  Copyright © 2018 Roopesh Chander. All rights reserved.
+//
+
+import UIKit
+
+class TunnelsListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "WireGuard"
+        let addButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped(sender:)))
+        self.navigationItem.rightBarButtonItem = addButtonItem
+    }
+
+    @objc func addButtonTapped(sender: UIBarButtonItem!) {
+        print("Add button tapped")
+    }
+}
+
+// MARK: UITableViewDataSource
+
+extension TunnelsListTableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
