052ece53a3a819351223a12593b9189e5fcb98d0
Roopesh Chander <roop@roopc.net>
2018-10-17 13:20:20 +0530

    Tunnels list: Make the table view style explicit

--- WireGuard/WireGuard/UI/iOS/TunnelsListTableViewController.swift
+++ WireGuard/WireGuard/UI/iOS/TunnelsListTableViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class TunnelsListTableViewController: UITableViewController {
 
+    init() {
+        super.init(style: .plain)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "WireGuard"
