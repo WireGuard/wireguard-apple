e1b8b6789024b8b89057a49871daa92b5090f3a2
Roopesh Chander <roop@roopc.net>
2018-10-23 16:23:27 +0530

    Model for DNS server
    Signed-off-by: Roopesh Chander <roop@roopc.net>

---
+++ WireGuard/WireGuard/Model/DNSServer.swift
@@ -0,0 +1,62 @@
+//
+//  DNSServer.swift
+//  WireGuard
+//
+//  Created by Roopesh Chander on 23/10/18.
+//  Copyright © 2018 WireGuard LLC. All rights reserved.
+//
+
+import Foundation
+import Network
+
+@available(OSX 10.14, iOS 12.0, *)
+struct DNSServer {
+    let address: IPAddress
+}
+
+// MARK: Converting to and from String
+// For use in the UI
+
+extension DNSServer {
+    init?(from addressString: String) {
+        if let addr = IPv4Address(addressString) {
+            address = addr
+        } else if let addr = IPv6Address(addressString) {
+            address = addr
+        } else {
+            return nil
+        }
+    }
+    func stringRepresentation() -> String {
+        return "\(address)"
+    }
+}
+
+// MARK: Codable
+// For serializing to disk
+
+@available(OSX 10.14, iOS 12.0, *)
+extension DNSServer: Codable {
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(address.rawValue)
+    }
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        var data = try container.decode(Data.self)
+        let ipAddressFromData: IPAddress? = {
+            switch (data.count) {
+            case 4: return IPv4Address(data)
+            case 16: return IPv6Address(data)
+            default: return nil
+            }
+        }()
+        guard let ipAddress = ipAddressFromData else {
+            throw DecodingError.invalidData
+        }
+        address = ipAddress
+    }
+    enum DecodingError: Error {
+        case invalidData
+    }
+}
