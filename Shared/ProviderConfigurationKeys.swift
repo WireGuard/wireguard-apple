80f7c880282e9d7f12d840d6e144bab330808c63
Roopesh Chander <roop@roopc.net>
2018-11-02 15:39:03 +0530

    Remove files we don't need

--- Shared/ProviderConfigurationKeys.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Copyright © 2018 WireGuard LLC. All Rights Reserved.
-//
-
-import Foundation
-
-enum PCKeys: String {
-    case title, tunnelIdentifier, endpoints, dns, addresses, mtu, settings
-}
