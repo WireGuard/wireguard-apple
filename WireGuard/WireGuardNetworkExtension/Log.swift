54cfa3dba3cee53d55a415f4898f86564c66cff3
Jason A. Donenfeld <Jason@zx2c4.com>
2018-11-04 04:16:13 +0100

    XCode: use old network extension ID

--- WireGuard/WireGuardNetworkExtension/Log.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Copyright © 2018 WireGuard LLC. All Rights Reserved.
-//
-
-import os.log
-
-struct Log {
-    static var general = OSLog(subsystem: "com.wireguard.ios.network-extension", category: "general")
-}
