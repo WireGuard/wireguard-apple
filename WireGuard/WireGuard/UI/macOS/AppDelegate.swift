5c7a149167802902c1632b8062df2969f4680b04
Roopesh Chander <roop@roopc.net>
2018-11-25 18:42:51 +0530

    macOS: Remove MainMenu.xib
    When there's no xib, we should explicitly set the app delegate, so we
    override NSApplication and set the app delegate in NSApplication.shared
    
    Signed-off-by: Roopesh Chander <roop@roopc.net>

--- WireGuard/WireGuard/UI/macOS/AppDelegate.swift
+++ WireGuard/WireGuard/UI/macOS/AppDelegate.swift
@@ -6,9 +6,6 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
