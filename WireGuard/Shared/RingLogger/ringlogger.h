6a12af4066f2df77ae7015ff526e87255402e2b2
Jason A. Donenfeld <Jason@zx2c4.com>
2018-12-13 05:51:03 +0100

    Commit untested ringlogger code

---
+++ WireGuard/Shared/RingLogger/ringlogger.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: MIT
+ *
+ * Copyright © 2018 WireGuard LLC. All Rights Reserved.
+ */
+
+#ifndef RINGLOGGER_H
+#define RINGLOGGER_H
+
+struct log;
+void write_msg_to_log(struct log *log, const char *msg);
+int write_logs_to_file(const char *file_name, const struct log *log1, const struct log *log2);
+struct log *open_log(const char *file_name);
+
+#endif
