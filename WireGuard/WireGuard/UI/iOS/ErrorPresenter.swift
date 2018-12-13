19f16c3291a9ef95a58d396105979ec7ddfa8f9f
Jason A. Donenfeld <Jason@zx2c4.com>
2018-12-14 00:01:50 +0100

    Deduplicate functions

--- WireGuard/WireGuard/UI/iOS/ErrorPresenter.swift
+++ WireGuard/WireGuard/UI/iOS/ErrorPresenter.swift
@@ -6,16 +6,8 @@ import os.log
 
 class ErrorPresenter {
     static func showErrorAlert(error: WireGuardAppError, from sourceVC: UIViewController?, onPresented: (() -> Void)? = nil, onDismissal: (() -> Void)? = nil) {
-        guard let sourceVC = sourceVC else { return }
-
         let (title, message) = error.alertText
-        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
-            onDismissal?()
-        }
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addAction(okAction)
-
-        sourceVC.present(alert, animated: true, completion: onPresented)
+        showErrorAlert(title: title, message: message, from: sourceVC, onPresented: onPresented, onDismissal: onDismissal)
     }
 
     static func showErrorAlert(title: String, message: String, from sourceVC: UIViewController?, onPresented: (() -> Void)? = nil, onDismissal: (() -> Void)? = nil) {
