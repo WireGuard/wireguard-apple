a5aaec673afd36d3044798f0f93a8a42c34a603f
Jason A. Donenfeld <Jason@zx2c4.com>
2018-12-13 19:43:12 +0100

    Avoid escaping heap allocation

--- WireGuard/WireGuardNetworkExtension/PacketTunnelProvider.swift
+++ WireGuard/WireGuardNetworkExtension/PacketTunnelProvider.swift
@@ -159,9 +159,9 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
 }
 
 // swiftlint:disable large_tuple
-func withStringsAsGoStrings<R>(_ s1: String, _ s2: String? = nil, _ s3: String? = nil, _ s4: String? = nil, call: @escaping ((gostring_t, gostring_t, gostring_t, gostring_t)) -> R) -> R {
-    func helper(_ p1: UnsafePointer<Int8>?, _ p2: UnsafePointer<Int8>?, _ p3: UnsafePointer<Int8>?, _ p4: UnsafePointer<Int8>?) -> R {
+func withStringsAsGoStrings<R>(_ s1: String, _ s2: String? = nil, _ s3: String? = nil, _ s4: String? = nil, call: ((gostring_t, gostring_t, gostring_t, gostring_t)) -> R) -> R {
+    func helper(_ p1: UnsafePointer<Int8>?, _ p2: UnsafePointer<Int8>?, _ p3: UnsafePointer<Int8>?, _ p4: UnsafePointer<Int8>?, _ call: ((gostring_t, gostring_t, gostring_t, gostring_t)) -> R) -> R {
         return call((gostring_t(p: p1, n: s1.utf8.count), gostring_t(p: p2, n: s2?.utf8.count ?? 0), gostring_t(p: p3, n: s3?.utf8.count ?? 0), gostring_t(p: p4, n: s4?.utf8.count ?? 0)))
     }
-    return helper(s1, s2, s3, s4)
+    return helper(s1, s2, s3, s4, call)
 }
