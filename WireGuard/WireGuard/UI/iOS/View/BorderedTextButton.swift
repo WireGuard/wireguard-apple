66fcdfd13c30d074213b621c9e64ad2e24edc72f
Eric Kuck <eric@bluelinelabs.com>
2018-12-14 20:02:37 -0600

    KeyValueCells now share code

--- WireGuard/WireGuard/UI/iOS/View/BorderedTextButton.swift
+++ WireGuard/WireGuard/UI/iOS/View/BorderedTextButton.swift
@@ -40,11 +40,12 @@ class BorderedTextButton: UIView {
         button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     @objc func buttonTapped() {
         onTapped?()
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
 }
---
+++ WireGuard/WireGuard/UI/iOS/View/TunnelEditKeyValueCell.swift
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+// Copyright © 2018 WireGuard LLC. All Rights Reserved.
+
+import UIKit
+
+class TunnelEditKeyValueCell: KeyValueCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        keyLabel.textAlignment = .right
+        valueTextField.textAlignment = .left
+        
+        let widthRatioConstraint = NSLayoutConstraint(item: keyLabel, attribute: .width, relatedBy: .equal, toItem: self, attribute: .width, multiplier: 0.4, constant: 0)
+        // In case the key doesn't fit into 0.4 * width,
+        // so set a CR priority > the 0.4-constraint's priority.
+        widthRatioConstraint.priority = .defaultHigh + 1
+        widthRatioConstraint.isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+class TunnelEditEditableKeyValueCell: TunnelEditKeyValueCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        valueTextField.textColor = .black
+        valueTextField.isEnabled = true
+        valueLabelScrollView.isScrollEnabled = false
+        valueTextField.widthAnchor.constraint(equalTo: valueLabelScrollView.widthAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
