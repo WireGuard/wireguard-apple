f6772dc3536698c7d1484ccf5811a4e3dd07ab8c
Roopesh Chander <roop@roopc.net>
2018-12-25 18:33:28 +0530

    macOS: Add WireGuardmacOS target
    Signed-off-by: Roopesh Chander <roop@roopc.net>

---
+++ WireGuard/WireGuardmacOS/AppDelegate.swift
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+// Copyright © 2018 WireGuard LLC. All Rights Reserved.
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
