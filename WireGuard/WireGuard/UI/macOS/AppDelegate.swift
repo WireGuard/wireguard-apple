04f6ee0f115bdeb26398568296ecef437872a660
Roopesh Chander <roop@roopc.net>
2018-12-29 18:44:29 +0530

    macOS: Ability to activate / deactivate a tunnel
    Signed-off-by: Roopesh Chander <roop@roopc.net>

--- WireGuard/WireGuard/UI/macOS/AppDelegate.swift
+++ WireGuard/WireGuard/UI/macOS/AppDelegate.swift
@@ -18,7 +18,26 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             self.statusItem = createStatusBarItem(with: statusMenu)
 
             tunnelsManager.tunnelsListDelegate = statusMenu
+            tunnelsManager.activationDelegate = self
         }
+    }
+}
+
+extension AppDelegate: TunnelsManagerActivationDelegate {
+    func tunnelActivationAttemptFailed(tunnel: TunnelContainer, error: TunnelsManagerActivationAttemptError) {
+        ErrorPresenter.showErrorAlert(error: error, from: nil)
+    }
+
+    func tunnelActivationAttemptSucceeded(tunnel: TunnelContainer) {
+        // Nothing to do
+    }
+
+    func tunnelActivationFailed(tunnel: TunnelContainer, error: TunnelsManagerActivationError) {
+        ErrorPresenter.showErrorAlert(error: error, from: nil)
+    }
+
+    func tunnelActivationSucceeded(tunnel: TunnelContainer) {
+        // Nothing to do
     }
 }
 
