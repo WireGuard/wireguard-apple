37d84f9f3b36654ac9c86a4efd94c9fc6298ca67
Roopesh Chander <roop@roopc.net>
2019-01-04 18:11:49 +0530

    macOS: Manage tunnels: Export log pulldown menu implementation

--- WireGuard/WireGuard/UI/macOS/ViewController/TunnelsListTableViewController.swift
+++ WireGuard/WireGuard/UI/macOS/ViewController/TunnelsListTableViewController.swift
@@ -144,7 +144,36 @@ class TunnelsListTableViewController: NSViewController {
     }
 
     @objc func exportLogClicked() {
-        print("exportLogClicked")
+        guard let window = view.window else { return }
+        let savePanel = NSSavePanel()
+        savePanel.prompt = "Save"
+        savePanel.nameFieldLabel = "Export log to"
+
+        let dateFormatter = ISO8601DateFormatter()
+        dateFormatter.formatOptions = [.withFullDate, .withTime, .withTimeZone] // Avoid ':' in the filename
+        let timeStampString = dateFormatter.string(from: Date())
+        savePanel.nameFieldStringValue = "wireguard-log-\(timeStampString).txt"
+
+        guard let networkExtensionLogFilePath = FileManager.networkExtensionLogFileURL?.path else {
+            ErrorPresenter.showErrorAlert(title: tr("alertUnableToFindExtensionLogPathTitle"), message: tr("alertUnableToFindExtensionLogPathMessage"), from: self)
+            return
+        }
+
+        savePanel.beginSheetModal(for: window) { response in
+            guard response == .OK else { return }
+            guard let destinationURL = savePanel.url else { return }
+
+            DispatchQueue.global(qos: .userInitiated).async {
+                let isWritten = Logger.global?.writeLog(called: "APP", mergedWith: networkExtensionLogFilePath, called: "NET", to: destinationURL.path) ?? false
+                guard isWritten else {
+                    DispatchQueue.main.async { [weak self] in
+                        ErrorPresenter.showErrorAlert(title: tr("alertUnableToWriteLogTitle"), message: tr("alertUnableToWriteLogMessage"), from: self)
+                    }
+                    return
+                }
+            }
+
+        }
     }
 
     @objc func exportTunnelsClicked() {
