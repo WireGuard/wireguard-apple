f6c9ce2d716f2f096be333bf5f1f1689a55f17b9
Roopesh Chander <roop@roopc.net>
2019-01-21 19:07:42 +0530

    macOS: Simplify NSColor extension
    Signed-off-by: Roopesh Chander <roop@roopc.net>

--- WireGuard/WireGuard/UI/macOS/NSColor+Hex.swift
+++ WireGuard/WireGuard/UI/macOS/NSColor+Hex.swift
@@ -19,7 +19,7 @@ extension NSColor {
         var rgb: UInt32 = 0
         Scanner(string: hexString).scanHexInt32(&rgb)
 
-        self.init(red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0, green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0, blue: CGFloat(rgb & 0x0000FF) / 255.0, alpha: 1)
+        self.init(red: CGFloat((rgb >> 16) & 0xff) / 255.0, green: CGFloat((rgb >> 8) & 0xff) / 255.0, blue: CGFloat((rgb >> 0) & 0xff) / 255.0, alpha: 1)
     }
 
 }
