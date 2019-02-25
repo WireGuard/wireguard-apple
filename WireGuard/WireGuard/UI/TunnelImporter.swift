c33563f8487955d986fb421365c3d324a7f996f1
Roopesh Chander <roop@roopc.net>
2019-02-25 16:22:52 +0530

    Importing: macOS: Support importing of multiple files at a time

--- WireGuard/WireGuard/UI/TunnelImporter.swift
+++ WireGuard/WireGuard/UI/TunnelImporter.swift
@@ -4,7 +4,45 @@
 import Foundation
 
 class TunnelImporter {
-    static func importFromFile(url: URL, into tunnelsManager: TunnelsManager, sourceVC: AnyObject?, errorPresenterType: ErrorPresenterProtocol.Type, completionHandler: (() -> Void)? = nil) {
+    static func importFromFile(urls: [URL], into tunnelsManager: TunnelsManager, sourceVC: AnyObject?, errorPresenterType: ErrorPresenterProtocol.Type, completionHandler: (() -> Void)? = nil) {
+        guard !urls.isEmpty else {
+            completionHandler?()
+            return
+        }
+        if urls.count > 1 {
+            let dispatchGroup = DispatchGroup()
+            var configs = [TunnelConfiguration?]()
+            for url in urls {
+                if url.pathExtension.lowercased() == "zip" {
+                    dispatchGroup.enter()
+                    ZipImporter.importConfigFiles(from: url) { result in
+                        if let configsInZip = result.value {
+                            configs.append(contentsOf: configsInZip)
+                        }
+                        dispatchGroup.leave()
+                    }
+                } else {
+                    let fileBaseName = url.deletingPathExtension().lastPathComponent.trimmingCharacters(in: .whitespacesAndNewlines)
+                    let fileContents = try? String(contentsOf: url)
+                    let tunnelConfiguration = try? TunnelConfiguration(fromWgQuickConfig: fileContents ?? "", called: fileBaseName)
+                    configs.append(tunnelConfiguration)
+                }
+            }
+            dispatchGroup.notify(queue: .main) {
+                tunnelsManager.addMultiple(tunnelConfigurations: configs.compactMap { $0 }) { numberSuccessful in
+                    if numberSuccessful == configs.count {
+                        completionHandler?()
+                        return
+                    }
+                    let title = tr(format: "alertImportedFromMultipleFilesTitle (%d)", numberSuccessful)
+                    let message = tr(format: "alertImportedFromMultipleFilesMessage (%1$d of %2$d)", numberSuccessful, configs.count)
+                    errorPresenterType.showErrorAlert(title: title, message: message, from: sourceVC, onPresented: completionHandler)
+                }
+            }
+            return
+        }
+        assert(urls.count == 1)
+        let url = urls.first!
         if url.pathExtension.lowercased() == "zip" {
             ZipImporter.importConfigFiles(from: url) { result in
                 if let error = result.error {
--- WireGuard/WireGuard/UI/macOS/ImportPanelPresenter.swift
+++ WireGuard/WireGuard/UI/macOS/ImportPanelPresenter.swift
@@ -9,12 +9,12 @@ class ImportPanelPresenter {
         let openPanel = NSOpenPanel()
         openPanel.prompt = tr("macSheetButtonImport")
         openPanel.allowedFileTypes = ["conf", "zip"]
+        openPanel.allowsMultipleSelection = true
         openPanel.beginSheetModal(for: window) { [weak tunnelsManager] response in
             guard let tunnelsManager = tunnelsManager else { return }
             guard response == .OK else { return }
-            guard let url = openPanel.url else { return }
             AppStorePrivacyNotice.show(from: sourceVC, into: tunnelsManager) {
-                TunnelImporter.importFromFile(url: url, into: tunnelsManager, sourceVC: sourceVC, errorPresenterType: ErrorPresenter.self)
+                TunnelImporter.importFromFile(urls: openPanel.urls, into: tunnelsManager, sourceVC: sourceVC, errorPresenterType: ErrorPresenter.self)
             }
         }
     }
