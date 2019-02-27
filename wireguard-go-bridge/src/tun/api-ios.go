06e40833479dd9a7c36d659effab51db4ec776a2
Jason A. Donenfeld <Jason@zx2c4.com>
2019-02-27 06:24:56 +0100

    wireguard-go-bridge: update to Go 1.12

--- wireguard-go-bridge/src/tun/api-ios.go
+++ wireguard-go-bridge/src/tun/api-ios.go
@@ -6,28 +6,24 @@
 package tun
 
 import (
-	"git.zx2c4.com/wireguard-go/rwcancel"
 	"golang.org/x/sys/unix"
 	"net"
 	"os"
 )
 
 func CreateTUNFromFD(tunFd int) (TUNDevice, string, error) {
-	file := os.NewFile(uintptr(tunFd), "/dev/tun")
-	tun := &nativeTun{
-		tunFile: file,
-		fd:      file.Fd(),
-		events:  make(chan TUNEvent, 5),
-		errors:  make(chan error, 5),
-	}
-	var err error
-	tun.rwcancel, err = rwcancel.NewRWCancel(tunFd)
+	err := unix.SetNonblock(tunFd, true)
 	if err != nil {
 		return nil, "", err
 	}
+	file := os.NewFile(uintptr(tunFd), "/dev/tun")
+	tun := &nativeTun{
+		tunFile: file,
+		events:  make(chan TUNEvent, 5),
+		errors:  make(chan error, 5),
+	}
 	name, err := tun.Name()
 	if err != nil {
-		tun.rwcancel.Cancel()
 		return nil, "", err
 	}
 	tunIfindex, err := func() (int, error) {
@@ -38,12 +34,10 @@ func CreateTUNFromFD(tunFd int) (TUNDevice, string, error) {
 		return iface.Index, nil
 	}()
 	if err != nil {
-		tun.tunFile.Close()
 		return nil, "", err
 	}
 	tun.routeSocket, err = unix.Socket(unix.AF_ROUTE, unix.SOCK_RAW, unix.AF_UNSPEC)
 	if err != nil {
-		tun.tunFile.Close()
 		return nil, "", err
 	}
 	go tun.routineRouteListener(tunIfindex)
