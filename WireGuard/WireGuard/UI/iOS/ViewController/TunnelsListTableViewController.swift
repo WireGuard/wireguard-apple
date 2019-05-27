WireGuard/wireguard-apple

commit 7322fb084087774e8b58e347902f6d7036cbde5c

iOS: Refactor showing of the tunnel detail

WireGuard/WireGuard/UI/iOS/ViewController/TunnelsListTableViewController.swift
@@ -251,6 +251,26 @@ class TunnelsListTableViewController: UIViewController {
             }
         }
     }
+
+    func showTunnelDetail(for tunnel: TunnelContainer, animated: Bool) {
+        guard let tunnelsManager = tunnelsManager else { return }
+        guard let splitViewController = splitViewController else { return }
+
+        if detailDisplayedTunnel != tunnel {
+            let tunnelDetailVC = TunnelDetailTableViewController(tunnelsManager: tunnelsManager,
+                                                                 tunnel: tunnel)
+            let tunnelDetailNC = UINavigationController(rootViewController: tunnelDetailVC)
+            tunnelDetailNC.restorationIdentifier = "DetailNC"
+            if animated {
+                splitViewController.showDetailViewController(tunnelDetailNC, sender: self)
+            } else {
+                UIView.performWithoutAnimation {
+                    splitViewController.showDetailViewController(tunnelDetailNC, sender: self)
+                }
+            }
+            detailDisplayedTunnel = tunnel
+        }
+    }
 }
 
 extension TunnelsListTableViewController: UIDocumentPickerDelegate {
@@ -309,12 +329,7 @@ extension TunnelsListTableViewController: UITableViewDelegate {
         }
         guard let tunnelsManager = tunnelsManager else { return }
         let tunnel = tunnelsManager.tunnel(at: indexPath.row)
-        let tunnelDetailVC = TunnelDetailTableViewController(tunnelsManager: tunnelsManager,
-                                                             tunnel: tunnel)
-        let tunnelDetailNC = UINavigationController(rootViewController: tunnelDetailVC)
-        tunnelDetailNC.restorationIdentifier = "DetailNC"
-        showDetailViewController(tunnelDetailNC, sender: self) // Shall get propagated up to the split-vc
-        detailDisplayedTunnel = tunnel
+        showTunnelDetail(for: tunnel, animated: true)
     }
 
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {

WireGuard/WireGuard/UI/iOS/ViewController/MainViewController.swift
@@ -90,19 +90,10 @@ extension MainViewController {
 
     func showTunnelDetailForTunnel(named tunnelName: String, animated: Bool, shouldToggleStatus: Bool) {
         let showTunnelDetailBlock: (TunnelsManager) -> Void = { [weak self] tunnelsManager in
+            guard let self = self else { return }
+            guard let tunnelsListVC = self.tunnelsListVC else { return }
             if let tunnel = tunnelsManager.tunnel(named: tunnelName) {
-                let tunnelDetailVC = TunnelDetailTableViewController(tunnelsManager: tunnelsManager, tunnel: tunnel)
-                let tunnelDetailNC = UINavigationController(rootViewController: tunnelDetailVC)
-                tunnelDetailNC.restorationIdentifier = "DetailNC"
-                if let self = self {
-                    if animated {
-                        self.showDetailViewController(tunnelDetailNC, sender: self)
-                    } else {
-                        UIView.performWithoutAnimation {
-                            self.showDetailViewController(tunnelDetailNC, sender: self)
-                        }
-                    }
-                }
+                tunnelsListVC.showTunnelDetail(for: tunnel, animated: false)
                 if shouldToggleStatus {
                     if tunnel.status == .inactive {
                         tunnelsManager.startActivation(of: tunnel)

commit 5e9780ef8fcb5fe7da7d30fc4c8df276547fc8dd

iOS: Should be able to re-show tunnel detail
Fixes a bug introduced in the refactoring in
commit 7322fb084087774e8b58e347902f6d7036cbde5c

Signed-off-by: Roopesh Chander <roop@roopc.net>

WireGuard/WireGuard/UI/iOS/ViewController/TunnelsListTableViewController.swift
@@ -257,18 +257,16 @@ class TunnelsListTableViewController: UIViewController {
         guard let splitViewController = splitViewController else { return }
         guard let navController = navigationController else { return }
 
-        if detailDisplayedTunnel != tunnel {
-            let tunnelDetailVC = TunnelDetailTableViewController(tunnelsManager: tunnelsManager,
-                                                                 tunnel: tunnel)
-            let tunnelDetailNC = UINavigationController(rootViewController: tunnelDetailVC)
-            tunnelDetailNC.restorationIdentifier = "DetailNC"
-            if splitViewController.isCollapsed && navController.viewControllers.count > 1 {
-                navController.setViewControllers([self, tunnelDetailNC], animated: animated)
-            } else {
-                splitViewController.showDetailViewController(tunnelDetailNC, sender: self, animated: animated)
-            }
-            detailDisplayedTunnel = tunnel
+        let tunnelDetailVC = TunnelDetailTableViewController(tunnelsManager: tunnelsManager,
+                                                             tunnel: tunnel)
+        let tunnelDetailNC = UINavigationController(rootViewController: tunnelDetailVC)
+        tunnelDetailNC.restorationIdentifier = "DetailNC"
+        if splitViewController.isCollapsed && navController.viewControllers.count > 1 {
+            navController.setViewControllers([self, tunnelDetailNC], animated: animated)
+        } else {
+            splitViewController.showDetailViewController(tunnelDetailNC, sender: self, animated: animated)
         }
+        detailDisplayedTunnel = tunnel
         self.presentedViewController?.dismiss(animated: false, completion: nil)
     }
 }