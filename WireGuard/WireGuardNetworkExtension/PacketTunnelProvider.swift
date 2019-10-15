9c4a9bd0487609da9e5862f8cdbfb66efa91e41b
Jason A. Donenfeld <Jason@zx2c4.com>
2019-10-15 16:51:50 +0200

    Revert "NetworkExtension: don't use exit(0) hack on Catalina"
    This reverts commit 3619279a65d9a506fb13d7f24909b38a5202fa8f.
    
    Still broken!

--- WireGuard/WireGuardNetworkExtension/PacketTunnelProvider.swift
+++ WireGuard/WireGuardNetworkExtension/PacketTunnelProvider.swift
@@ -99,10 +99,7 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
         // HACK: This is a filthy hack to work around Apple bug 32073323 (dup'd by us as 47526107).
         // Remove it when they finally fix this upstream and the fix has been rolled out to
         // sufficient quantities of users.
-        let osVersion = ProcessInfo.processInfo.operatingSystemVersion
-        if osVersion.majorVersion <= 10 && osVersion.minorVersion <= 14 {
-            exit(0)
-        }
+        exit(0)
         #endif
     }
 
