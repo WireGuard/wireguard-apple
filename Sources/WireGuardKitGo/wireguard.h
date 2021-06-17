13b720442d47b67a18d6841aa34569633a22b458
Jason A. Donenfeld <Jason@zx2c4.com>
2021-06-17 16:56:46 +0200

    Global: bump copyright year
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- Sources/WireGuardKitGo/wireguard.h
+++ Sources/WireGuardKitGo/wireguard.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0
  *
- * Copyright (C) 2018-2020 WireGuard LLC. All Rights Reserved.
+ * Copyright (C) 2018-2021 WireGuard LLC. All Rights Reserved.
  */
 
 #ifndef WIREGUARD_H
