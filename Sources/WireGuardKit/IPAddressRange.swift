977ec25e79e8e4e667d82dd79e3df63bde849103
Andrej Mihajlov <and@mullvad.net>
2022-09-26 16:43:17 +0200

    Kit: implement Codable for IPAddressRange
    Signed-off-by: Andrej Mihajlov <and@mullvad.net>

--- Sources/WireGuardKit/IPAddressRange.swift
+++ Sources/WireGuardKit/IPAddressRange.swift
@@ -27,6 +27,29 @@ extension IPAddressRange: Hashable {
     }
 }
 
+extension IPAddressRange: Codable {
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+
+        try container.encode(self.stringRepresentation)
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let value = try container.decode(String.self)
+
+        if let ipAddressRange = IPAddressRange(from: value) {
+            self = ipAddressRange
+        } else {
+            let context = DecodingError.Context(
+                codingPath: container.codingPath,
+                debugDescription: "Invalid IPAddressRange representation"
+            )
+            throw DecodingError.dataCorrupted(context)
+        }
+    }
+}
+
 extension IPAddressRange {
     public var stringRepresentation: String {
         return "\(address)/\(networkPrefixLength)"
