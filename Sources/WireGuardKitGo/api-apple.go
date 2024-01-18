de5de3911f4a16a3ada69998ffe22d08d62ba4ea
Igor Sorokin <is@amnezia.org>
2024-01-18 18:27:50 +0300

    Add links to amnezia-wg

--- Sources/WireGuardKitGo/api-apple.go
+++ Sources/WireGuardKitGo/api-apple.go
@@ -25,9 +25,9 @@ import (
 	"unsafe"
 
 	"golang.org/x/sys/unix"
-	"golang.zx2c4.com/wireguard/conn"
-	"golang.zx2c4.com/wireguard/device"
-	"golang.zx2c4.com/wireguard/tun"
+	"github.com/amnezia-vpn/amnezia-wg/conn"
+	"github.com/amnezia-vpn/amnezia-wg/device"
+	"github.com/amnezia-vpn/amnezia-wg/tun"
 )
 
 var loggerFunc unsafe.Pointer
@@ -209,7 +209,7 @@ func wgVersion() *C.char {
 		return C.CString("unknown")
 	}
 	for _, dep := range info.Deps {
-		if dep.Path == "golang.zx2c4.com/wireguard" {
+		if dep.Path == "github.com/amnezia-vpn/amnezia-wg" {
 			parts := strings.Split(dep.Version, "-")
 			if len(parts) == 3 && len(parts[2]) == 12 {
 				return C.CString(parts[2][:7])
